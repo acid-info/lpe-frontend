957e37bf6936e4bb77d79e660c08bb4194bf8146
jinhojang6 <jinho5051@gmail.com>
2023-04-19 20:11:21 +0900

    feat: add viewport setting to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import useIsDarkState from "@/states/isDarkState/isDarkState";
 import { defaultThemes, ThemeProvider } from "@acid-info/lsd-react";
 import { css, Global } from "@emotion/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   const isDark = useIsDarkState().get();
@@ -9,6 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={isDark ? defaultThemes.dark : defaultThemes.light}>
       <Component {...pageProps} />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </Head>
       <Global
         styles={css`
           html,
