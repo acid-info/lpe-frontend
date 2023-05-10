8fd015c95f1f2a9ef42126e2da5a5e7929216c96
jinhojang6 <jinho5051@gmail.com>
2023-05-10 21:29:16 +0900

    feat: add og image

---
+++ src/pages/api/og.tsx
@@ -0,0 +1,38 @@
+import { ImageResponse } from '@vercel/og'
+
+export const config = {
+  runtime: 'edge',
+}
+
+const image = fetch(new URL('public/og.png', import.meta.url)).then((res) =>
+  res.arrayBuffer(),
+)
+
+// Doc: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation/og-image-examples#using-a-local-image
+export default async function handler() {
+  const imageData = await image
+  const blob = new Blob([imageData])
+  // Doc has type error
+  const srcBlob = URL.createObjectURL(blob)
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img width="1200" height="630" src={srcBlob} />
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  )
+}
