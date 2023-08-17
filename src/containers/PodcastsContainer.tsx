760c3685952d8ab35924eed7d1bb32a9024be5b1
jinhojang6 <jinho5051@gmail.com>
2023-08-17 20:59:27 +0900

    style: update podcasts style

--- src/containers/PodcastsContainer.tsx
+++ src/containers/PodcastsContainer.tsx
@@ -3,8 +3,11 @@ import styled from '@emotion/styled'
 import { LPE } from '../types/lpe.types'
 import PodcastsList from '@/components/Podcasts/Podcasts.List'
 import EpisodesList from '@/components/Podcasts/Episodes.List'
-import { Typography } from '@acid-info/lsd-react'
+import { Button, Typography } from '@acid-info/lsd-react'
 import { PodcastType } from '@/components/Post/Post'
+import { LogosCircleIcon } from '@/components/Icons/LogosCircleIcon'
+import Link from 'next/link'
+import { HashingItOutIcon } from '@/components/Icons/HashingItOutIcon'
 
 interface Props {
   shows: LPE.Podcast.Show[]
@@ -14,6 +17,12 @@ interface Props {
 const PodcastsContainer = (props: Props) => {
   const { shows, latestEpisodes } = props
 
+  const networkState =
+    shows.find((show) => show.slug === PodcastType.NETWORK_STATE) ?? shows[0]
+
+  const hashingItOut =
+    shows.find((show) => show.slug === PodcastType.HASHING_IT_OUT) ?? shows[1]
+
   return (
     <PodcastsGrid>
       <PodcastsBodyContainer className={'w-16'}>
@@ -21,7 +30,7 @@ const PodcastsContainer = (props: Props) => {
 
         <Section>
           <EpisodesList
-            header={<Typography variant="body2">Latest Episodes</Typography>}
+            header={<EpisodeListHeader>Latest Episodes</EpisodeListHeader>}
             episodes={latestEpisodes}
             podcastType={PodcastType.LATEST}
           />
@@ -29,7 +38,24 @@ const PodcastsContainer = (props: Props) => {
 
         <Section>
           <EpisodesList
-            header={<Typography variant="body2">State of Network</Typography>}
+            header={
+              <EpisodeListHeader>
+                <Show>
+                  <LogosCircleIcon width={38} height={38} />
+                  <PodcastInfo>
+                    <Typography variant="body1">
+                      {networkState.title}
+                    </Typography>
+                    <Typography variant="body3">
+                      {networkState.numberOfEpisodes} EP
+                    </Typography>
+                  </PodcastInfo>
+                </Show>
+                <Link href={`/podcasts/${networkState.slug}`}>
+                  <Button size="small">See all episodes</Button>
+                </Link>
+              </EpisodeListHeader>
+            }
             episodes={latestEpisodes.slice(0, 4)}
             podcastType={PodcastType.NETWORK_STATE}
           />
@@ -37,7 +63,24 @@ const PodcastsContainer = (props: Props) => {
 
         <Section>
           <EpisodesList
-            header={<Typography variant="body2">Hashing It Out</Typography>}
+            header={
+              <EpisodeListHeader>
+                <Show>
+                  <HashingItOutIcon width={38} height={38} />
+                  <PodcastInfo>
+                    <Typography variant="body1">
+                      {hashingItOut.title}
+                    </Typography>
+                    <Typography variant="body3">
+                      {hashingItOut.numberOfEpisodes} EP
+                    </Typography>
+                  </PodcastInfo>
+                </Show>
+                <Link href={`/podcasts/${hashingItOut.slug}`}>
+                  <Button size="small">See all episodes</Button>
+                </Link>
+              </EpisodeListHeader>
+            }
             episodes={latestEpisodes.slice(0, 4)}
             podcastType={PodcastType.HASHING_IT_OUT}
           />
@@ -61,6 +104,25 @@ const PodcastsGrid = styled(Grid)`
 
 const Section = styled.div`
   margin-top: 140px;
+`
+
+const EpisodeListHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const Show = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`
+
+const PodcastInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
 `
 
 export default PodcastsContainer
