71bd962cb3175bacbcaf791e16a339d046a6f4c1
amirhouieh <amir@status.im>
2023-08-25 11:02:38 +0200

    navbar links typography adjustment

--- src/components/NavBar/Navbar.Links.tsx
+++ src/components/NavBar/Navbar.Links.tsx
@@ -1,18 +1,28 @@
 import Link from 'next/link'
 import styled from '@emotion/styled'
+import { Typography } from '@acid-info/lsd-react'
+import { useRouter } from 'next/router'
 
 interface Props {
   links: { href: string; label: string }[]
 }
 
 export const NavbarLinks = ({ links }: Props) => {
+  const router = useRouter()
+  const { pathname } = router
   return (
     <Container>
       {links.map((link, idx) => (
         <>
-          <Link href={link.href} key={`navbar-link-${idx}`}>
-            {link.label}
-          </Link>
+          <Typography variant={'label2'}>
+            <Link
+              href={link.href}
+              key={`navbar-link-${idx}`}
+              className={pathname === link.href ? 'active' : ''}
+            >
+              {link.label}
+            </Link>
+          </Typography>
           {idx !== links.length - 1 && <span className={'divider'} />}
         </>
       ))}
@@ -24,8 +34,13 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
   a {
     text-decoration: none;
+  }
+
+  a:hover, a:active, a:focus, a.active {
+    text-decoration: underline;
   }
 
   .divider {
@@ -38,7 +53,7 @@ const Container = styled.div`
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.sm.width}px) {
-    flex-direction: column;
+    flex -direction: column;
     align-items: flex-start;
     .divider {
       display: none;
