dab25d06d9bbc388a7b0d3ac19ce23b27920dcd7
amirhouieh <amir@status.im>
2023-08-29 17:58:52 +0200

    [#122] capitalize author name

--- src/components/Article/Article.ImageBlockWrapper.tsx
+++ src/components/Article/Article.ImageBlockWrapper.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import { LPE } from '../../types/lpe.types'
 import { ResponsiveImage } from '../ResponsiveImage/ResponsiveImage'
+import { lsdUtils } from '@/utils/lsd.utils'
 
 type Props = {
   image: LPE.Image.Document
@@ -10,8 +11,16 @@ export const ArticleImageBlockWrapper = ({ image }: Props) => {
   return (
     <Container>
       <ResponsiveImage data={image} />
+      <figcaption>{image.alt}</figcaption>
     </Container>
   )
 }
 
-const Container = styled.div``
+const Container = styled.figure`
+  margin: 0;
+  padding: 0;
+  figcaption {
+    padding-top: 8px;
+    ${lsdUtils.typography('body3')}
+  }
+`
