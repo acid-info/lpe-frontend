3d1a46bffc63b5cbbfda5c5b5c9c68c4df179a96
Hossein Mehrabi <ohyeaross@gmail.com>
2023-08-31 16:14:23 +0330

    fix: disable webhook on Vercel

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -2,6 +2,7 @@ import { readFile, writeFile } from 'fs/promises'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import path from 'path'
 
+const IS_VERCEL = process.env.VERCEL === '1'
 const WEBHOOK_DATA_PATH = path.join(__dirname, '../../webhook_data.json')
 const TOKEN =
   process.env.REVALIDATE_WEBHOOK_TOKEN ||
@@ -18,7 +19,7 @@ export const getWebhookData = async (): Promise<WebhookData> =>
   JSON.parse((await readFile(WEBHOOK_DATA_PATH, 'utf-8')) || '{}')
 
 let initialized = false
-if (!initialized) writeWebhookData({ lastUpdate: +new Date() })
+if (IS_VERCEL && !initialized) writeWebhookData({ lastUpdate: +new Date() })
 
 export default async function handler(
   req: NextApiRequest,
@@ -27,6 +28,8 @@ export default async function handler(
   const {
     query: { token = '' },
   } = req
+
+  if (IS_VERCEL) return res.status(404).json({ message: 'Not found!' })
 
   if (token !== TOKEN) return res.status(401).json({ message: 'Invalid token' })
 
